c812cfe5da989bbac76c9cf31c95f84ae4252529
seninp <seninp@gmail.com>
2015-11-27 09:31:45 +0100

    finalizing the table

--- RCode/make_KO_table.R
+++ RCode/make_KO_table.R
@@ -43,5 +43,49 @@ for (i in c(2:(length(tags$id))) ) {
   names(res)[length(names(res))] <- paste(tag[2])
   write.table(res, file = "table_progress.csv", quote = T, col.names = T, row.names = F, sep = "\t")
 }
-
-
+#
+#
+# define a function that extracts the KO description and first 10 MAPS
+map_titles = function(ko_id) {
+  
+  desc <- dbGetQuery(session, paste(
+  "select description from ko_description where ko_id=",shQuote(ko_id),";",sep = ""))
+  
+  maps <- dbGetQuery(session, paste(
+  "select mt.title from ko_map km 
+  join map_title mt on km.map_id=mt.map_id 
+  where ko_id=", shQuote(substring(ko_id,4)),";",sep = ""))
+  
+  sequence <- rep("",10)
+  if (dim(maps)[1] > 0) {
+    maps <- as.vector(t(maps))
+    if (length(maps) > 10) {
+      sequence <- maps[1:10]
+    } else {
+      sequence[1:length(maps)] <- maps
+    }
+  }
+  
+  c(unlist(desc),unlist(sequence))
+}
+#
+# test the function
+map_titles("ko:K17989")
+#
+#
+# extract these for all the KOs
+kos <- res[,1]
+#
+tmp_set <- matrix( rep("",12), nrow = 1)
+for (ko_id in kos) {
+  tmp <- map_titles(ko_id)
+  print(paste(ko_id))
+  tmp_set <- rbind( tmp_set, matrix(c(ko_id,tmp),nrow = 1) )
+}
+#
+# combine the both tables and write down the results
+tmp_set <- tmp_set[-1,]
+colnames(tmp_set) <- c("ko_id","ko_description",paste("pathway_",1:10,sep =  ""))
+res2 <- merge(res, tmp_set, by.x = c("ko_id"), by.y = c("ko_id"), all = T)
+write.table(res2,file = "table_all_ko_noeuc_tresholded_trimmed.csv", 
+            quote = T, col.names = T, row.names = F, sep = "\t")
